2c1b32f5eac9e2ef1f70a0d271ccac988ef2629a
Alexander Mora Ruiz <alex@creci.co>
2022-08-23 19:24:36 -0500

    add sample functions

--- src/logica/coleccion.py
+++ src/logica/coleccion.py
@@ -35,3 +35,46 @@ class Coleccion():
     def dar_album_por_id(self, album_id):
         return session.query(Album).get(album_id).__dict__
 
+    def obtener_canciones(self):
+        canciones = session.query(Cancion).all()
+
+        print('Las canciones almacenadas son:')
+        for cancion in canciones:
+            print("Titulo: " + cancion.titulo + " (00:" +
+                    str(cancion.minutos) + ":" +
+                    str(cancion.segundos) + ")")
+
+            print("Intérpretes")
+            for interprete in cancion.interpretes:
+                print(" - " + interprete.nombre)
+
+            for album in cancion.albumes:
+                print(" -- Presente en el album: " + album.titulo)
+
+            print("")
+
+
+        print('Los álbumes almacenados en discos son:')
+        albumes = session.query(Album).filter(Album.medio == Medio.DISCO).all()
+        for album in albumes:
+            print("Album: " + album.titulo)
+
+        # session.close()
+
+    def actualizar_cancion(self):
+        cancion = session.query(Cancion).get(2)
+        interprete = session.query(Interprete).get(4)
+
+        cancion.minutos = 5
+        cancion.segundos = 30
+        cancion.compositor = "Pedro Pérez"
+        cancion.interpretes.append(interprete)
+        session.add(cancion)
+        session.commit()
+        # session.close()
+
+    def eliminar_cancion(self, cancion_id):
+        cancion = session.query(Cancion).get(cancion_id)
+        session.delete(cancion)
+        session.commit()
+        # session.close()
